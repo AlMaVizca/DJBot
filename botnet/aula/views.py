a8db5ef61faf99bd5e65e0fb720456c18301a253
krahser <aldo.vizcaino87@gmail.com>
2013-05-15 18:26:36 -0300

    Recodificando

--- botnet/aula/views.py
+++ botnet/aula/views.py
@@ -1,24 +1,37 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from botnet.aula.models import Computadora, Tarea
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from botnet.aula.forms import FormularioEjecutar
+from botnet.aula.models import Computadora
+from django.shortcuts import render
+from botnet.aula.forms import FormularioAulas, FormularioListaTareas
 from botnet.aula.funciones import *
 
 
 @login_required
-def ejecutar(request, listaDeTareas):
-    tareas = listaDeTareas.split(',')
-    lista = []
-    for each in tareas:
-        lista.append(Tarea.objects.get(nombre=each))
-    ejecutarFormulario = FormularioEjecutar(initial={'tareas': listaDeTareas})
-    contexto = RequestContext(request, {
-        'tareas': lista,
-        'formulario': ejecutarFormulario,
-        })
-    return render_to_response('botnet/ejecutar.html', contexto)
+def ejecutar(request, lista_de_tareas=None):
+    form_aula = FormularioAulas(request.POST or None)
+    form_tareas = FormularioListaTareas(request.POST or None)
+    if form_aula.is_valid() and form_tareas.is_valid():
+        valores = form_aula.cleaned_data
+        valores = dict(form_tareas.cleaned_data.items() + valores.items())
+        ips = [compu.ip for each in valores['aulas']
+            for compu in Computadora.objects.filter(aula=each)]
+        borrar_archivo()
+        ejecutar_tareas(valores['tareas'], ips)
+        ejecutado = salida_computadora()
+        contexto = {'tareas': valores['tareas'],
+            'aula': valores['aulas'][0],
+            'computadoras': ips,
+            'ejecutado': ejecutado,
+            }
+        return render(request, 'botnet/ejecutando.html', contexto)
+    tareas = lista_de_tareas.split(',')
+    formularioTareas = FormularioListaTareas(
+    initial={'tareas': tareas})
+    contexto = {
+        'tareas': formularioTareas,
+        'formulario': FormularioAulas(),
+    }
+    return render(request, 'botnet/ejecutar.html', contexto)
 
 
 @login_required
@@ -26,20 +39,6 @@ def ejecutando(request):
     if request.method == 'POST':
         formulario = FormularioEjecutar(request.POST)
         if formulario.is_valid():
-            valores = formulario.cleaned_data
-            nombreAula = valores['aulas']
-            tareas = valores['tareas'].split(',')
-            computadoras = Computadora.objects.filter(aula=nombreAula)
-            ips = escribir_ips(computadoras)
-            borrar_archivo()
-            ejecutar_tareas(tareas, ips)
-            ejecutado = salida_computadora()
-            contexto = Context({
-                'tareas': tareas,
-                'aula': nombreAula,
-                'computadoras': computadoras,
-                'ejecutado': ejecutado,
-                })
             return render_to_response('botnet/ejecutando.html', contexto)
     else:
             return HttpResponse('<h1>Estas haciendo las cosas bien?</h1>')
