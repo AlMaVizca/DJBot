84a1305c4943cfb844f73305e9855e46d8617be7
krahser <aldo.vizcaino87@gmail.com>
2013-05-15 18:26:36 -0300

    Recodificando

--- botnet/urls.py
+++ botnet/urls.py
@@ -7,9 +7,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    #(?P<listaDeTareas>[\[,\],\',\w,\s]*)
-    url(r'^ejecutar/(?P<listaDeTareas>[\[,\],\',\w,\s]*)',
+    url(r'^ejecutar/(?P<lista_de_tareas>[\[,\],\',\w,\s]*)',
              'botnet.aula.views.ejecutar', name="ejecutar"),
+    url(r'^ejecutar/', 'botnet.aula.views.ejecutar', name="ejecutar"),
     url(r'^prender/(?P<listaDeSalas>[\[,\],\',\w,\s]*)',
         'botnet.aula.views.prender', name="prender"),
     url(r'^ejecutando/$', 'botnet.aula.views.ejecutando', name="ejecutado"),
