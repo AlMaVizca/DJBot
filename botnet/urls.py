8f80c76124ba2b089fbfe6cee984d7f62d58607d
krahser <aldo.vizcaino87@gmail.com>
2013-05-20 17:59:49 -0300

    enlaces

--- botnet/urls.py
+++ botnet/urls.py
@@ -1,15 +1,14 @@
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^ejecutar/', 'botnet.aula.views.ejecutar', name="ejecutar"),
     url(r'^ejecutar/(?P<lista_de_tareas>[\[,\],\',\w,\s]*)',
              'botnet.aula.views.ejecutar', name="ejecutar"),
-    url(r'^ejecutar/', 'botnet.aula.views.ejecutar', name="ejecutar"),
     url(r'^prender/(?P<listaDeSalas>[\[,\],\',\w,\s]*)',
         'botnet.aula.views.prender', name="prender"),
     url(r'^django-rq/', include('django_rq.urls')),
