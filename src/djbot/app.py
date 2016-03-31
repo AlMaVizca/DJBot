2cf6a8581d83b68eef712185ecb5930eda8c4644
krahser <aldo.vizcaino87@gmail.com>
2016-03-31 02:59:21 -0300

    add routes for playbooks and inventory

--- src/djbot/app.py
+++ src/djbot/app.py
@@ -10,8 +10,18 @@ app.config.from_object(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    host = { 'name': 'uno', 'cpu': 'dos'}
+    return render_template('home.html', host=host)
+
+@app.route('/playbooks/', methods=['GET', 'POST'])
+def playbooks():
     page = {'error': False}
-    return render_template('home.html', page=page)
+    return render_template('playbooks.html', page=page)
+
+@app.route('/inventory/', methods=['GET', 'POST'])
+def inventory():
+    page = {'error': False}
+    return render_template('inventory.html', page=page)
 
 
 if __name__ == '__main__':
