cd8fa4ffb71006e051ed866ca19b0cf08305ac91
krahser <aldo.vizcaino87@gmail.com>
2016-05-07 00:18:10 -0300

    lastest modifications, sorry

--- README.md
+++ README.md
@@ -1,32 +1,11 @@
 Djbot
 ======
 
-Sistema de control remoto de equipos basado en SSH.
+¿Qué es?
+-------
 
-Podes encontrar una mejor descripción [aquí](http://sedici.unlp.edu.ar/handle/10915/31339)
+Un sistema de administracion para salas de computadoras o cibers que utilizan el sistema operativo linux.
+Por sistema de administracion se entiende que DjBot es capaz de encender, instalar programas y configurar servicios en muchas computadoras.
 
-#Requisitos
-* [Django 1.4.5](https://www.djangoproject.com/ "django")
-* [Fabric](http://docs.fabfile.org/en/1.6/)
-* [django-admin-bootstrapped](http://riccardo.forina.me/bootstrap-your-django-admin-in-3-minutes/)
-* [Redis](http://redis.io/)
-* [django-rq](https://github.com/ui/django-rq/)
-* [django-redis](https://django-redis.readthedocs.org/en/latest/)
+DjBot es una interfaz grafica construido sobre la plataforma de ansible version 2.
 
-
-Instalar <a id="instalar"/>
---------
-
-El desarrollo del sistema se llevo a cabo con [virtualenv](https://pypi.python.org/pypi/virtualenv) y se instalaron las dependencias con pip.
-
-
-    virtualenv envdjbot
-    source <path envdjbot>/bin/activate
-    pip install django==1.4.8 fabric django-admin-bootstrapped django-rq django-redis
-    python manage.py syncdb
-  
-Para ejecutar el proyecto tenemos que instalar Redis y ejecutar redis-server con las configuracion que se encuentra en la carpeta de djbot.
-
-
-    python manage.py rqworker default ejecutar
-    python manage.py runserver
--- src/djbot/app.py
+++ src/djbot/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, render_template, request
-
+from context import get_rooms, get_host
+from ansibleapi import *
 
 DEBUG = True
 app = Flask(__name__)
@@ -10,19 +11,42 @@ app.config.from_object(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    host = { 'name': 'uno', 'cpu': 'dos'}
-    return render_template('home.html', host=host)
+    computer_rooms = get_rooms()
+    return render_template('dashboard.html', computer_rooms=computer_rooms)
 
-@app.route('/playbooks/', methods=['GET', 'POST'])
-def playbooks():
-    page = {'error': False}
-    return render_template('playbooks.html', page=page)
+@app.route('/settings/<room_name>', methods=['GET', 'POST'])
+def settings(room_name):
+    return render_template('room.html', room_name=room_name)
 
-@app.route('/inventory/', methods=['GET', 'POST'])
-def inventory():
-    page = {'error': False}
-    return render_template('inventory.html', page=page)
+    
+@app.route('/room/<room_name>', methods=['GET', 'POST'])
+def room(room_name):
+    host = get_host()
+    inventory = ['localhost','163.10.78.1']
+    ansible_game = Runner(inventory)
+    ansible_game.add_play('shell log',['163.10.78.1'])
+    ansible_game.run()
+    host = ansible_game.get_setup()
+    return render_template('room.html', room_name=room_name, host=host)
 
+@app.route('/task/<room_name>', methods=['GET', 'POST'])
+def task(room_name):
+    return render_template('task.html', room_name=room_name)
 
+@app.route('/task/add/<room_name>', methods=['GET', 'POST'])
+def add_task(room_name):
+    return render_template('task.html', room_name=room_name)
+
+@app.route('/task/edit/<room_name>', methods=['GET', 'POST'])
+def edit_task(room_name):
+    return render_template('task.html', room_name=room_name)
+    
+@app.route('/task/delete/<room_name>', methods=['GET', 'POST'])
+def delete_task(room_name):
+    return render_template('task.html', room_name=room_name)
+    
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
