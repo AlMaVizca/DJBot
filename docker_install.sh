8d451f5333c885a319e1c959c5be41ef9cac8912
krahser <aldo.vizcaino87@gmail.com>
2016-06-15 13:36:49 -0300

    improve verbosity

--- docker_install.sh
+++ docker_install.sh
@@ -5,7 +5,10 @@ apt-get install apt-transport-https ca-certificates -yqq
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list
-apt-get update
+echo '### apt-get update ---> this may take a while...'
+apt-get update -qq
+echo '### apt-get install docker-engine ---> this may take a while...'
 apt-get install docker-engine -yqq
+echo '### systemctl enable docker && systemctl start docker'
 systemctl enable docker
 systemctl start docker
