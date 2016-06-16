939edf056741fce14ed71ada1a0bbbba1fe4c301
krahser <aldo.vizcaino87@gmail.com>
2016-06-16 19:46:00 -0300

    add docker-compose for room use cases

---
+++ room_example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+mkdir /tmp/DJBot
+pushd /tmp/DJBot
+
+
+wget https://github.com/krahser/DJBot/blob/dev/docker-compose.yml
+
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
+
+docker-compose up -d
+
+docker-compose scale computer=10
+
+
+echo "The network is:"
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' djbot_computer_2
