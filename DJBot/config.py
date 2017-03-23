934d17b72d91a7d2027a51ea6f40e95444d4b05c
krahser <aldo.vizcaino87@gmail.com>
2017-03-23 15:12:38 +0800

    remove ignore file in pytest and improve configurations

--- DJBot/config.py
+++ DJBot/config.py
@@ -1,20 +1,42 @@
 import os
+import tempfile
 
-TESTING = False
-DEBUG = False
 
-DATABASE = 'djbot.db'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.basename(DATABASE)
+class Config():
+    TESTING = False
+    DEBUG = False
 
-CSRF_ENABLED = True
+    SECRET_KEY = 'changeThisKey'
+    CSRF_ENABLED = True
+    # Override the database path
+    DATABASE = ''
 
-# Flask-security settings
-SECURITY_PASSWORD_HASH = "bcrypt"
-SECURITY_TRACKABLE = True
-SECURITY_USER_IDENTITY_ATTRIBUTES = ['email', 'username']
-SECURITY_PASSWORD_SALT = "changeThisInInstanceConfiguration"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
 
-# Flask-User settings
-USER_APP_NAME = "DJBot"
-USER_ENABLE_REGISTRATION = False
-DEBUG_TB_INTERCEPT_REDIRECTS = False
+    # Flask-security settings
+    SECURITY_PASSWORD_HASH = "bcrypt"
+    SECURITY_TRACKABLE = True
+    SECURITY_USER_IDENTITY_ATTRIBUTES = ['email', 'username']
+    SECURITY_PASSWORD_SALT = "changeThisInInstanceConfiguration"
+
+    # Flask-User settings
+    USER_APP_NAME = "DJBot"
+    USER_ENABLE_REGISTRATION = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+
+class Production(Config):
+    DATABASE = os.path.basename('djbot.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
+
+
+class Testing(Config):
+    f, path = tempfile.mkstemp(
+        prefix='flask-security-test-db', suffix='.db', dir='/tmp')
+
+    TESTING = True
+    DEBUG = True
+    USER_APP_NAME = "DJBot-Testing"
+
+    DATABASE = path
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
