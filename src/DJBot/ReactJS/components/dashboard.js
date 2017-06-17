2a3a07fe2a0569ebb7666e31fca053f395e70393
krahser <aldo.vizcaino87@gmail.com>
2017-06-17 13:13:34 +0800

    add dashboard

--- src/DJBot/ReactJS/components/dashboard.js
+++ src/DJBot/ReactJS/components/dashboard.js
@@ -1,12 +1,55 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
+import { Grid, Header, Icon, Segment } from 'semantic-ui-react';
 
 var Dashboard = React.createClass({
   render: function () {
-    var Grid = Semantify.Grid;
     return (
-        <div>
-         <p>'hello'</p>
-        </div>
+      <Grid centered columns="equal" textAlign="center" divided>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Segment textAlign="center" name="Playbooks"
+                     as={Link} to="/playbooks">
+              <Icon name="play" size="big" />
+              <Icon name="book" size="big" />
+              <Header>Playbooks</Header>
+              {this.props.playbooks > 1 ? (
+                There are {this.props.playbooks} available.
+              ) : (
+                There is {this.props.playbooks} available.
+              )}
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="center" name="Inventory"
+                     as={Link} to="/inventory">
+              <Icon name="sitemap" size="big" />
+              <Header>Inventory</Header>
+              {this.props.inventory > 1 ? (
+                There are {this.props.inventory} units.
+              ) : (
+                There is {this.props.inventory} unit.
+              )}
+
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="center" name="Results"
+                     as={Link} to="/results">
+              <Icon name="wordpress forms" size="big" />
+              <Header>Results</Header>
+              {this.props.results > 1 ? (
+                There were {this.props.results} executions.
+              ) : (
+                There was {this.props.results} execution.
+              )}
+            </Segment>
+          </Grid.Column>
+
+        </Grid.Row>
+      </Grid>
     )
   }
 });
--- src/DJBot/views/settings.py
+++ src/DJBot/views/settings.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from flask_security import login_required, roles_required
 from DJBot.forms.generic import SelectName
 from DJBot.utils.ssh import generate_key, remove_key
+from DJBot.models.playbook import Playbook
+from DJBot.models.room import Room
 import os
 import re
 
@@ -12,6 +14,19 @@ def get_keys():
     keys = os.listdir(os.getenv("HOME")+"/.ssh/keys")
     keys = [each for each in keys if not re.match(".*\.pub", each)]
     return keys
+
+
+def get_results():
+    return os.listdir(os.getenv("LOGS"))
+
+
+@settings_bp.route('/main', methods=['GET'])
+@login_required
+@roles_required('user')
+def main():
+    return jsonify({"playbooks": Playbook.query.count(),
+                    "inventory": Room.query.count(),
+                    "results": len(get_results())})
 
 
 @settings_bp.route('/keys', methods=['GET'])
